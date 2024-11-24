93a2fb8717a08561d28e18b4c1d5b0a75b109314
kev1nandreas <kevin.andreascn@gmail.com>
2024-11-25 00:41:27 +0700

    update database connection strings in TaskDAO and UserDAO to use new proxy host

--- src/main/java/usermanagement/dao/UserDAO.java
+++ src/main/java/usermanagement/dao/UserDAO.java
@@ -16,7 +16,7 @@ public class UserDAO {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             // Define the database URL, username, and password
-            String url = "jdbc:mysql://mysql.railway.internal:3306/railway";
+            String url = "jdbc:mysql://junction.proxy.rlwy.net:41386/railway";
             String user = "root";
             String password = "wcXzdUiNIUDswxShyEIELHseiyjoqMYR";
 
